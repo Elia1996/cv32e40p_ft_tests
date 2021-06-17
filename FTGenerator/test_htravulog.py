ef4254bc3e7aaa90179c5a96a33a6e66b15f1a8b
elia <ribaldoneelia@gmail.com>
2021-06-17 10:56:00 +0200

    Script to execute test on htravulog

--- FTGenerator/test_htravulog.py
+++ FTGenerator/test_htravulog.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-import htravulog
+from  htravulog import *
 
+htv = htravulog("/home/tesla/Desktop/Tesi/Esecuzione_tesi/cv32e40p_ft_tests/FTGenerator/test/arch/cv32e40p_if_stage.sv")
 
+htv.ElaborateHiddenTravulog()
